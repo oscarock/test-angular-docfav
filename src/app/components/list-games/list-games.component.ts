f6f2dfb9aa18f5364e13343f4bbc9fd3e474b08a
Oscar Diaz <oscarock02@gmail.com>
2023-12-23 18:42:14 -0500

    ajustes componenete lista de jeugos

--- src/app/components/list-games/list-games.component.ts
+++ src/app/components/list-games/list-games.component.ts
@@ -1,65 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+import { FormFiltersComponent } from '../form-filters/form-filters.component';
 import { SearchService } from '../../services/search.service';
-import { map, switchMap } from 'rxjs/operators';
-import { ApiServiceMethods } from '../../services/api.service.types';
+import { map, switchMap, distinctUntilChanged, takeUntil, startWith } from 'rxjs/operators';
+import { RouterModule } from '@angular/router';
+import { Subject, Observable, combineLatest, of } from 'rxjs';
 
 @Component({
   selector: 'app-list-games',
+  standalone: true,
+  imports: [FormFiltersComponent,RouterModule],
   templateUrl: './list-games.component.html',
-  styleUrls: ['./list-games.component.css']
+  styleUrl: './list-games.component.css',
 })
-export class ListGamesComponent implements OnInit {
+export class ListGamesComponent implements OnInit, OnDestroy {
   games: any;
+  private destroy$: Subject<void> = new Subject<void>();
 
-  constructor(
-    private service: ApiService,
-    private searchService: SearchService
-  ) {}
+  constructor(private service: ApiService, private searchService: SearchService) {}
 
   ngOnInit() {
-    this.searchService.currentSearchName
-      .pipe(
-        switchMap(searchName => this.getFilteredGames('title', searchName))
-      )
-      .subscribe(response => this.games = response);
-
-    this.searchService.currentSearchGenre
-      .pipe(
-        switchMap(searchGenre => this.getFilteredGames('genre', searchGenre))
-      )
-      .subscribe(response => this.games = response);
-
-    this.searchService.currentSearchPlatform
-      .pipe(
-        switchMap(searchPlatform => this.getFilteredGames('platform', searchPlatform))
-      )
-      .subscribe(response => this.games = response);
+    this.subscribeToSearchChanges('currentSearchName', 'title');
+    this.subscribeToSearchChanges('currentSearchGenre', 'genre');
+    this.subscribeToSearchChanges('currentSearchPlatform', 'platform');
   }
 
-  private getFilteredGames(filterType: string, filterValue: string) {
-    const filterMethod = this.getFilterMethod(filterType);
-
-    return (this.service as any)[filterMethod](filterValue)
-      .pipe(
-        map(response => this.extractGamesFromResponse(response))
-      );
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
-  private getFilterMethod(filterType: string): keyof ApiServiceMethods {
-    switch (filterType) {
+  private subscribeToSearchChanges(searchType: string, propertyName: string) {
+    const searchService = this.searchService as any;
+    searchService[searchType].pipe(
+      distinctUntilChanged(),
+      switchMap((searchValue: string) => {
+        if (searchValue) {
+          if (propertyName === 'title') {
+            return this.loadGames(searchValue);
+          } else {
+            return this.filterGames(searchValue, propertyName);
+          }
+        } else {
+          return this.loadGames(); // Cargar todos los juegos cuando el valor de búsqueda es vacío
+        }
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe((response: any) => {
+      this.games = response;
+    });
+  }
+
+  private loadGames(title?: string): Observable<any> {
+    return this.service.getAllGames().pipe(
+      map(response => this.extractGamesFromResponse(response)),
+      map((games: any[]) => {
+        if (title) {
+          return games.filter(game => game.title.toLowerCase().includes(title.toLowerCase()));
+        }
+        return games;
+      })
+    );
+  }
+
+  private filterGames(searchValue: string, propertyName: string): Observable<any> {
+    let filterObservable: Observable<any>;
+
+    switch (propertyName) {
       case 'title':
-        return 'getAllGames';
+        filterObservable = this.loadGames(); // Llamamos a loadGames para cargar todos los juegos
+        break;
       case 'genre':
-        return 'getFilterGenre';
+        filterObservable = this.service.getFilterGenre(searchValue);
+        break;
       case 'platform':
-        return 'getFilterPlatform';
+        filterObservable = this.service.getFilterPlatform(searchValue);
+        break;
       default:
-        return 'getAllGames';
+        throw new Error('Nombre de propiedad no válido');
     }
+
+    return filterObservable.pipe(
+      map(response => this.extractGamesFromResponse(response as any[]))
+    );
   }
 
   private extractGamesFromResponse(response: any): any[] {
     return response;
+  }
+
+  clearSearch(): void {
+    this.searchService.changeSearchName('');
   }
 }
